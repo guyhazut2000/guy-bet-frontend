b6a734c3412ba8bfa0d4919461bfa4af5b5f2534
Guy <guyhazut2000@gmail.com>
2023-02-22 10:51:19 +0200

    update project deployment url

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "http://localhost:5000/api",
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://project-clientserver.herokuapp.com/api"
+      ? "https://guy-bet-api.onrender.com/api/v1"
       : "http://localhost:5000/api/v1",
   headers: {
     "Content-type": "application/json",
